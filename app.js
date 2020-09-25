fafb246de9c8715962e3cd842204ff2c20849952
Jesse Smart <jessemillersmart@gmail.com>
2020-09-25 11:33:32 -0400

    render functions filled, handle error function added

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const store = {
       answers: [
         'Generates sound',
         'Generates a clock signal',
-        'Pass all frequencies below a specified cutoff',
+        'Passes all frequencies below a specified cutoff',
         'Defines how a given sound will change over time'
       ],
       correctAnswer: 0
@@ -65,27 +65,27 @@ const store = {
  * displays the title
  * provides a start butoon which the user can press to begin the quiz*/
 function startTemplate() {
-  return `<div class="card">
+  return `<div>
   <h2>Press Play to Begin the Sequence</h2>
-  <form id="begin">
-  <input type="submit" value="Play"></input>
+  <form id="begin" class="start-form">
+  <input type="submit" value="Play" class="play-button"></input>
   </div>`;
 }
 
 /**Template Function - creates the question and answer pages*/
 function questionTemplate(item) {
-  return `<div class="card">
+  return `<div>
   <h2>${item.question}</h2>
   <form id="choices">
   <input type="radio" id="${item.answers[0]}" name="answer" value="${item.answers[0]}">
-  <label class="answrbtn" for="answer0">${item.answers[0]}</label><br>
+  <label class="answr-btn" for="answer0">${item.answers[0]}</label><br>
   <input type="radio" id="${item.answers[1]}" name="answer" value="${item.answers[1]}">
-  <label class="answrbtn" for="answer1">${item.answers[1]}</label><br>
+  <label class="answr-btn" for="answer1">${item.answers[1]}</label><br>
   <input type="radio" id="${item.answers[2]}" name="answer" value="${item.answers[2]}">
-  <label class="answrbtn" for="answer2">${item.answers[2]}</label>
+  <label class="answr-btn" for="answer2">${item.answers[2]}</label>
   <input type="radio" id="${item.answers[3]}" name="answer" value="${item.answers[3]}">
-  <label class="answrbtn" for="answer3">${item.answers[3]}</label>
-  <button type="submit">Submit</button>
+  <label class="answr-btn" for="answer3">${item.answers[3]}</label>
+  <button type="submit">Play</button>
   </form>
 </div>`;
 }
@@ -114,27 +114,39 @@ function handleAnswer() {
 
 }
 
+/**Event Handler -  */
+function handleError() {
+
+}
+
 /**Render Function - renders the start page*/
 function renderStart() {
-
+  template = startTemplate();
+  $('main').html(template);
 }
 
 function renderQuestion() {
-
+  template = questionTemplate();
+  return $('main').html(template);
 }
 
 function renderResponse() {
-
+  template = responseTemplate();
+  return $('main').html(template);
 }
 
 function renderFinal() {
-
+  template = finalTemplate();
+  return $('main').html(template);
 }
 
 /**gestalt function which activates the event handlers 
  * and then renders the startpage (where everything comes together)*/
 function main() {
-
+handleStart();
+handleAnswer();
+handleError();
+renderStart();
 }
 /**executes main function */
 $(main);
